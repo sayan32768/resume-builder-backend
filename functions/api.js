d359a6eccb4da3fe22d6c34b103f7aff9647eb80
Sayan Adhikari <sayanadh123@gmail.com>
2025-10-29 23:48:42 +0530

    changes v3

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import 'dotenv/config';
-import connectDB from "../database/db.js";
+// import connectDB from "../database/db.js";
 import userRoute from '../routes/userRoute.js';
 import resumeRoute from '../routes/resumeRoute.js';
 import cors from "cors";
@@ -9,7 +9,16 @@ import ServerlessHttp from "serverless-http";
 
 const app = express();
 
-connectDB();
+// connectDB();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/resume-builder`)
+        console.log('MongoDB connected successfully!')
+    } catch (e) {
+        console.log(`MongoDB Connection Error, ${e}`)
+    }
+})()
 
 app.use(express.json());
 app.use(cookieParser());
