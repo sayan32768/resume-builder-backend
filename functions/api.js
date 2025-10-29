9a14a54d24c825b5606a35b7dd7ad0464a1de0eb
Sayan Adhikari <sayanadh123@gmail.com>
2025-10-29 23:44:44 +0530

    changes v2

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,6 @@
 import express from "express";
 import 'dotenv/config';
-import connectDB from "../../database/db.js";
+import connectDB from "../database/db.js";
 import userRoute from '../routes/userRoute.js';
 import resumeRoute from '../routes/resumeRoute.js';
 import cors from "cors";
